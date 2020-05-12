6c735eed0f761bae02c7a72fb286111a09e330fd
Fw[a]rd <howard.main@outlook.com>
2020-05-13 00:46:28 +0800

    加入 warehouse 单元测试和实验性的Enum OrderStatus
    - warehouse test 通过
    - OrderStatus 仍需修改

---
+++ Warehouse-Server/src/main/java/org/server/entity/OrderStatus.java
@@ -0,0 +1,46 @@
+package org.server.entity;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public 
+enum OrderStatus {
+    NORMAL("正常"), DISCARD("报废");
+
+    private String status;
+    private OrderStatus(String status) {
+        if (validate(status)) {
+            this.status = status;
+        }
+        else {
+            // 随便搬来的，意思不大一样，记得换一个，比方EnumValidateException
+            throw new EnumConstantNotPresentException(OrderStatus.class, status);
+        }
+    }
+
+    public static Boolean validate(String status) {
+        if (status.equals("正常") || status.equals("报废")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void setStatus(String status) {
+        if (validate(status)) {
+            this.status = status;
+        }
+        else {
+            throw new EnumConstantNotPresentException(OrderStatus.class, status);
+        }
+    }
+    @JsonValue
+    public String getStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
+}
---
+++ Warehouse-Server/src/test/java/org/server/WarehouseTest.java
@@ -0,0 +1,51 @@
+package org.server;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.server.entity.Product;
+import org.server.entity.Warehouse;
+import org.server.mapper.WarehouseMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StopWatch;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class WarehouseTest {
+
+    @Resource
+    private WarehouseMapper warehouseMapper;
+
+    @Test
+    void getWarehouseList() {
+        StopWatch stopwatch = new StopWatch();
+        stopwatch.start();
+        List<Warehouse> list = warehouseMapper.getWarehouseList(null);
+        stopwatch.stop();
+        System.out.println(stopwatch.getLastTaskTimeMillis());
+        System.out.println(list);
+    }
+
+    @Test
+    void addWarehouse() {
+        Warehouse warehouse = new Warehouse();
+        warehouse.setName("合肥工业大仓库");
+        warehouseMapper.addWarehouse(warehouse);
+    }
+
+    @Test
+    void getProductListById() {
+        StopWatch stopwatch = new StopWatch();
+        stopwatch.start();
+        List<Product> list = warehouseMapper.getProductListById(1);
+        stopwatch.stop();
+        System.out.println(stopwatch.getLastTaskTimeMillis());
+        System.out.println(list);
+    }
+    
+    // passed, nothing failed. 
+
+}
