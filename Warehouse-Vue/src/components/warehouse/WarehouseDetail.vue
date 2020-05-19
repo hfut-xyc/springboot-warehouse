335831866238fdd75e37999076c2f8eb50764dfd
Fw[a]rd <howard.main@outlook.com>
2020-05-19 15:19:38 +0800

    Warehouse 添加负责人分页
    - 略微有点奇怪，下次改成选择员工之后不移除的那种

--- Warehouse-Vue/src/components/warehouse/WarehouseDetail.vue
+++ Warehouse-Vue/src/components/warehouse/WarehouseDetail.vue
@@ -55,12 +55,20 @@
               placement="bottom"
               width="400"
               trigger="click">
-              <el-table :data="employeeListSelection" ref="employeeTable" @selection-change="handleEmployeeListSelction">
+              <el-table :data="employeeListSelection" v-loading="employeeLoading" ref="employeeTable" @selection-change="handleEmployeeListSelction">
                 <el-table-column type="selection"></el-table-column>
-                <el-table-column property="id" label="工号"></el-table-column>
+                <el-table-column property="id" label="工号" width="100px"></el-table-column>
                 <el-table-column property="name" label="姓名"></el-table-column>
               </el-table>
-              <el-button slot="reference" icon="el-icon-plus" size="small" type="primary" @click="updateEmployeeListSelction()">添加员工</el-button>
+              <el-pagination
+                layout="prev, pager, next"
+                @current-change="onEmployeePageChange"
+                :total="totalEmployee"
+                page-size="5"
+                style="text-align: center;"
+              >
+              </el-pagination>
+              <el-button slot="reference" :loading="employeeLoading" icon="el-icon-plus" size="small" type="primary" @click="updateEmployeeListSelction()">添加员工</el-button>
             </el-popover>
             </span>
             <el-button icon="el-icon-check" size="small" @click="updateWarehouseEmployee()" type="success">确定</el-button>
@@ -73,7 +81,7 @@
           <template slot="title">
             <i class="el-icon-box"></i>仓库存储清单
           </template>
-          <el-table :data="productList" v-loading="loading" border stripe>
+          <el-table :data="productList" v-loading="productLoading" border stripe>
             <el-table-column prop="id" label="产品编号" sortable width="150"></el-table-column>
             <el-table-column prop="name" label="产品名"></el-table-column>
             <el-table-column prop="supplier" label="供应商"></el-table-column>
@@ -81,8 +89,8 @@
           </el-table>
           <el-pagination
             layout="prev, pager, next"
-            @current-change="onPageChange"
-            :total="totalPages">
+            @current-change="onProductPageChange"
+            :total="totalProduct">
           </el-pagination>
         </el-collapse-item>
       </el-collapse>
@@ -101,21 +109,29 @@
           callback()
       };
       return {
+        // 路由接收的参数，包括id, name, operators
         info: {},
+        // 产品列表
         productList: [],
+        // 雇员列表，懒加载
         employeeList: [],
+        // 用于选择的雇员列表，会根据选择的情况进行列表的添加和移除操作
         employeeListSelection: [],
+        // el-collapse 默认打开的 item
         activeCollapseItems: ['基本信息', '仓库负责人', '仓库存储清单'],
 
-        loading: false,
-        isEditingEmployees: false,
-        totalPages: 0,
+        
+        productLoading: false, // 产品列表是否在加载
+        employeeLoading: false, // 雇员列表是否在加载
+        isEditingEmployees: false, // 是否在编辑雇员
+        totalProduct: 0, // 总计产品数量
+        totalEmployee: 0, // 总计员工数量
 
-        updateForm: {
+        updateForm: { // 修改仓库名称的Form
           id: 0,
           name: ""
         },
-        rules: {
+        rules: { // 表单验证器
           name: [{validator: warehouseNameValidator, trigger: "blur"}]
         },
       }
@@ -134,43 +150,50 @@
       },
 
       loadProductList(url) {
-        this.loading = true;
+        this.productLoading = true;
         var that = this;
         this.$axios.get(url).then(res => {
           if (res.status === 200) {
             console.log(res);
             that.productList = res.data.productList;
-            that.totalPages = res.data.total;
-            that.loading = false;
+            that.totalProduct = res.data.total;
+            that.productLoading = false;
             that.$message.success("数据加载成功");
           } else {
-            that.loading = false;
+            that.productLoading = false;
             that.$message.error("数据加载失败");
           }
         }).catch(res => {
           console.log(res);
-          that.loading = false;
+          that.productLoading = false;
           that.$message.error("服务器异常");
         });
       },
 
       loadEmployeeList(url) {
+        this.employeeLoading = true;
         let that = this;
         this.$axios.get(url).then(res => {
           if (res.status === 200) {
             console.log(res);
             that.employeeList = res.data.employeeList;
+            that.totalEmployee = res.data.total;
             // 深拷贝，此处为万不得以，修改了会导致没法恢复员工列表
             that.employeeListSelection = JSON.parse(JSON.stringify(res.data.employeeList));
+            this.updateEmployeeListSelction();
+            that.employeeLoading = false;
           } else {
             that.$message.error("数据加载失败");
+            that.employeeLoading = false;
           }
         }).catch(res => {
           console.log(res);
           that.$message.error("服务器异常");
+          that.employeeLoading = false;
         });
       },
 
+      // 在打开选择气泡前，对employeeListSelction进行的筛除操作
       updateEmployeeListSelction() {
         this.info.operators.forEach((employee, index, array) => {
           this.employeeListSelection.find((v, i, obj) => {
@@ -183,6 +206,7 @@
         });
       },
 
+      // 在气泡中选中某(几)行，对employeeListSelction进行的筛除操作
       handleEmployeeListSelction(vals) {
         this.info.operators = this.info.operators.concat(vals);
         vals.forEach((employee, index, array) => {
@@ -198,12 +222,41 @@
       },
 
       // 商品分页
-      onPageChange(val) {
-        this.productLoading = true;
-        let url = "/warehouse/" + this.info.id + "/products?page=" + val;
+      onProductPageChange(val) {
+        let url = `/warehouse/${this.info.id}/products?page=${val}`;
         this.loadProductList(url);
       },
 
+      // 雇员分页
+      onEmployeePageChange(val) {
+        let url = `/employees?page=${val}&pageSize=5`;
+        this.loadEmployeeList(url);
+      },
+
+      // “编辑”按钮绑定的事件
+      editEmployeeTag() {
+        this.isEditingEmployees = true;
+        this.loadEmployeeList("/employees?pageSize=5");
+      },
+
+      // Tag删除触发的事件
+      deleteEmployeeTag(eid) {
+        this.info.operators.find((value, index, obj) => { 
+          if(value.id == eid) {
+            this.info.operators.splice(index, 1);
+            this.employeeList.find((v, i, o) => {
+              if (v.id == eid) {
+                this.employeeListSelection.push(v);
+                this.employeeListSelection.sort((a, b) => a.id - b.id);
+                return true;
+              }
+            });
+            return true;
+          }
+        });
+      },
+
+      // 更新 Warehouse 基本信息
       updateWarehouseInfo() {
         this.$refs["updateForm"].validate(valid => {
           if (valid) {
@@ -234,27 +287,7 @@
         });
       },
 
-      editEmployeeTag() {
-        this.isEditingEmployees = true;
-        this.loadEmployeeList("/employees");
-      },
-
-      deleteEmployeeTag(eid) {
-        this.info.operators.find((value, index, obj) => { 
-          if(value.id == eid) {
-            this.info.operators.splice(index, 1);
-            this.employeeList.find((v, i, o) => {
-              if (v.id == eid) {
-                this.employeeListSelection.push(v);
-                this.employeeListSelection.sort((a, b) => a.id - b.id);
-                return true;
-              }
-            });
-            return true;
-          }
-        });
-      },
-
+      // 更新 Warehouse 负责人信息
       updateWarehouseEmployee() {
         let that = this;
         let eidList = [];
